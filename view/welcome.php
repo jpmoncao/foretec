87b784670deba32a0baeccebb6a3fb472c3b9a6f
João Pedro Monção <pedrohjoao44@gmail.com>
2023-02-13 17:07:00 -0300

    📦 NEW: Adding homepage to an external file

---
+++ view/welcome.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ForEtec | A plataforma que veio para auxiliar os etequianos!</title>
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+
+<body>
+    <header>
+        <h1>For<span class="title-red">Etec</span></h1>
+    </header>
+
+    <section>
+        <img src="assets/img/illustration-raising-hand-study.svg" alt="imagem de uma pessoa estudando">
+        <div>
+            <h1>A plataforma que veio para auxiliar os etequianos!</h1>
+            <p>Acesse com o seu RA e data de nascimento</p>
+
+            <a href="?page=login"><button>cadastre-se agora</button></a>
+            <a href="?page=acess"><button class="acess">já tenho uma conta</button></a>
+        </div>
+    </section>
+
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci optio aliquam cupiditate magnam tempore repellendus cum hic! Beatae amet voluptate necessitatibus! Labore, nostrum dolor. Saepe rerum omnis unde illo. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, doloribus. Necessitatibus quam minima sit excepturi possimus! Modi et a temporibus dolorem iste placeat nam neque quia voluptatem aperiam? Nisi, sequi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque aliquid vel possimus officiis dicta id nostrum voluptatem rem dolorem, pariatur, molestiae voluptas, deleniti saepe sed doloribus tempora nisi. Recusandae, optio!</h1>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci optio aliquam cupiditate magnam tempore repellendus cum hic! Beatae amet voluptate necessitatibus! Labore, nostrum dolor. Saepe rerum omnis unde illo. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, doloribus. Necessitatibus quam minima sit excepturi possimus! Modi et a temporibus dolorem iste placeat nam neque quia voluptatem aperiam? Nisi, sequi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque aliquid vel possimus officiis dicta id nostrum voluptatem rem dolorem, pariatur, molestiae voluptas, deleniti saepe sed doloribus tempora nisi. Recusandae, optio!</h1>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci optio aliquam cupiditate magnam tempore repellendus cum hic! Beatae amet voluptate necessitatibus! Labore, nostrum dolor. Saepe rerum omnis unde illo. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, doloribus. Necessitatibus quam minima sit excepturi possimus! Modi et a temporibus dolorem iste placeat nam neque quia voluptatem aperiam? Nisi, sequi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque aliquid vel possimus officiis dicta id nostrum voluptatem rem dolorem, pariatur, molestiae voluptas, deleniti saepe sed doloribus tempora nisi. Recusandae, optio!</h1>
+</body>
+
+</html>

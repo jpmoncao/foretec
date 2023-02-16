911a922399b20f53ecbae794feb55e2871683951
João Pedro Monção <pedrohjoao44@gmail.com>
2023-02-15 23:48:02 -0300

    🚀 RELEASE: Continuing the rest of the homepage

--- view/welcome.php
+++ view/welcome.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ForEtec | A plataforma que veio para auxiliar os etequianos!</title>
-    
+
     <!-- STYLESHEET -->
     <link rel="stylesheet" href="assets/css/index.css">
 
@@ -19,7 +19,7 @@
         <h1>For<span class="title-red">Etec</span></h1>
     </header>
 
-    <section>
+    <section class="first-content">
         <img src="assets/img/illustration-raising-hand-study.svg" alt="imagem de uma pessoa estudando">
         <div>
             <h1>A plataforma que veio para auxiliar os etequianos!</h1>
@@ -30,9 +30,31 @@
         </div>
     </section>
 
+    <section class="secondary">
+        <article class="method">
+            <img src="assets/img/icon-book.png" alt="Imagem de um livro aberto com um fundo redondo em vermelho">
+            <div>
+                <h2>O método mais divertido do mundo de estudar</h2>
+                <p>Estude de maneira fácil e divertida com a plataforma For<span class="title-red">Etec</span>! Com lições rápidas e interativas atribuidas pelos seus próprios professores.</p>
+            </div>
+        </article>
+        <div class="hr"></div>
+        <article class="who-are">
+            <div>
+                <h2>Quem somos</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, libero harum nesciunt quasi autem numquam dolor amet qui nostrum aperiam quae ducimus nobis corporis fugit, molestiae commodi officia sunt ipsa?</p>
+            </div>
+            <img src="assets/img/icon-logo.png" alt="Imagem de um círculo com o logo no centro e um chapéu universitário em cima">
+            <div>
+                <h2>Nosso objetivo</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, libero harum nesciunt quasi autem numquam dolor amet qui nostrum aperiam quae ducimus nobis corporis fugit, molestiae commodi officia sunt ipsa?</p>
+            </div>
+        </article>
+    </section>
+
+    <!-- <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci optio aliquam cupiditate magnam tempore repellendus cum hic! Beatae amet voluptate necessitatibus! Labore, nostrum dolor. Saepe rerum omnis unde illo. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, doloribus. Necessitatibus quam minima sit excepturi possimus! Modi et a temporibus dolorem iste placeat nam neque quia voluptatem aperiam? Nisi, sequi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque aliquid vel possimus officiis dicta id nostrum voluptatem rem dolorem, pariatur, molestiae voluptas, deleniti saepe sed doloribus tempora nisi. Recusandae, optio!</h1>
     <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci optio aliquam cupiditate magnam tempore repellendus cum hic! Beatae amet voluptate necessitatibus! Labore, nostrum dolor. Saepe rerum omnis unde illo. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, doloribus. Necessitatibus quam minima sit excepturi possimus! Modi et a temporibus dolorem iste placeat nam neque quia voluptatem aperiam? Nisi, sequi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque aliquid vel possimus officiis dicta id nostrum voluptatem rem dolorem, pariatur, molestiae voluptas, deleniti saepe sed doloribus tempora nisi. Recusandae, optio!</h1>
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci optio aliquam cupiditate magnam tempore repellendus cum hic! Beatae amet voluptate necessitatibus! Labore, nostrum dolor. Saepe rerum omnis unde illo. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, doloribus. Necessitatibus quam minima sit excepturi possimus! Modi et a temporibus dolorem iste placeat nam neque quia voluptatem aperiam? Nisi, sequi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque aliquid vel possimus officiis dicta id nostrum voluptatem rem dolorem, pariatur, molestiae voluptas, deleniti saepe sed doloribus tempora nisi. Recusandae, optio!</h1>
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci optio aliquam cupiditate magnam tempore repellendus cum hic! Beatae amet voluptate necessitatibus! Labore, nostrum dolor. Saepe rerum omnis unde illo. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, doloribus. Necessitatibus quam minima sit excepturi possimus! Modi et a temporibus dolorem iste placeat nam neque quia voluptatem aperiam? Nisi, sequi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque aliquid vel possimus officiis dicta id nostrum voluptatem rem dolorem, pariatur, molestiae voluptas, deleniti saepe sed doloribus tempora nisi. Recusandae, optio!</h1>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci optio aliquam cupiditate magnam tempore repellendus cum hic! Beatae amet voluptate necessitatibus! Labore, nostrum dolor. Saepe rerum omnis unde illo. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, doloribus. Necessitatibus quam minima sit excepturi possimus! Modi et a temporibus dolorem iste placeat nam neque quia voluptatem aperiam? Nisi, sequi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque aliquid vel possimus officiis dicta id nostrum voluptatem rem dolorem, pariatur, molestiae voluptas, deleniti saepe sed doloribus tempora nisi. Recusandae, optio!</h1> -->
 </body>
 
 </html>

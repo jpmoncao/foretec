e82c56c62b0eebeae6bddc0fa056a7a57ab1b8ce
João Pedro Monção <pedrohjoao44@gmail.com>
2023-03-14 17:42:05 -0300

    👌 IMPROVE: Error checking database connection

--- src/config.php
+++ src/config.php
@@ -4,4 +4,11 @@ $username = "root";
 $password = "123456";
 $dbname = "foretec";
 
+// Criar conexão com o banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexão
+if ($conn->connect_error) {
+    die("Erro ao conectar ao banco de dados: " . $conn->connect_error);
+}
+?>
